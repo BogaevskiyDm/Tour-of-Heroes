e494fc369b9211094596adf85d715ee57fac7919
Dmitriy <bogaevskiy.dmitriy@mail.ru>
2021-08-28 22:15:45 +0300

    Add Services

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,16 +4,12 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.scss'],
 })
-
 export class HeroDetailComponent implements OnInit {
-
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
